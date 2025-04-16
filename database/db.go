96fffa57e150c6875843ed2793ec10b392732edb
ZishunD <gaozishun.david@gmail.com>
2025-04-16 15:00:50 +0700

    docker version

--- database/db.go
+++ database/db.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -18,17 +17,7 @@ func ConnectDB() {
 	}
 
 	// 从环境变量读取
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-	sslmode := os.Getenv("SSL_MODE")
-
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		host, user, password, dbname, port, sslmode,
-	)
+	dsn := os.Getenv("DB_URL")
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
